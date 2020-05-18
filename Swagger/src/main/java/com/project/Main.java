7180dc51fe6416d7590eace0ea90e5de937d3986
HEHANG999 <3139210087@qq.com>
2020-05-18 17:06:25 +0800

    何航：上传swagger2

--- Swagger/src/main/java/com/project/Main.java
+++ Swagger/src/main/java/com/project/Main.java
@@ -3,6 +3,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 页面访问：http://127.0.0.1:8080/swagger-ui.html
+ */
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Main {
 
